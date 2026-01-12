82c87b66b13579d27e9d13353f6cdab1213e6c21
Lorena <lorenahernandez@examplemaths.com>
2026-01-12 19:36:28 +0100

    Texto añadido por Lorena > En el siguiente fichero estará la tabla del 13.

---
+++ TablaDel12.java
@@ -0,0 +1,16 @@
+public class TablaDel12
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+    { 
+        System.out.println(">Se ha imprimido por pantalla la tabla del 12.");
+    }
+    {
+        System.out.println("> En el siguiente fichero estará la tabla del 13.");
+    }
+}
