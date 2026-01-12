780823fc5c438002a3dbaf0a36cd926c56dd82f3
Jorge <jorgarher4@alu.edu.gva.es>
2026-01-12 19:44:11 +0100

    Cambio incorporado Jorge > Desarrollado para la aplicación Maths.

---
+++ TablaDel12.java
@@ -0,0 +1,16 @@
+public class TablaDel12
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+    { 
+        System.out.println(">Se ha imprimido por pantalla la tabla del 12.");
+    }
+    {
+        System.out.println("> Desarrollado para la aplicación Maths.");
+    }
+}
