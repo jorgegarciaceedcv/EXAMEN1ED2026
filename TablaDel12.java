3732d8ed156cbd93d3e4315eeae1913ba63252bf
Juan <juan.garcia@examplemaths.com>
2026-01-12 19:03:44 +0100

    Texto devuelto >Esta es la tabla del 12

---
+++ TablaDel12.java
@@ -0,0 +1,13 @@
+public class TablaDel12
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+    { 
+        System.out.println(">Esta es la tabla del 12");
+    }
+}
